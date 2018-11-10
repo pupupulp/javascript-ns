2d769b47c6fccca7bd6ca24863b4d362cfe0b3a8
Eagan Martin <mece.martinece@gmail.com>
2018-11-10 18:58:12 +0800

    Added wrappers for client events;

--- NS.js
+++ NS.js
@@ -10,5 +10,104 @@
 
 /** @type {Object} Main namespace for the package/library */
 var NS = {
-	
-}
+	/**
+	 * All wrappers for client scripts
+	 * @type {Object}
+	 */
+	client: {
+		/**
+		 * [pageInit description]
+		 * @param  {[type]}   type     ['create', 'copy', 'edit']
+		 * @param  {Function} callback [description]
+		 * @return {[type]}            [description]
+		 */
+		eventPageInit: function(type, callback) {
+			return callback(type);
+		},
+		/**
+		 * [eventSaveRecord description]
+		 * @param  {Function} callback [description]
+		 * @return {[type]}            [description]
+		 */
+		eventSaveRecord: function(callback) {
+			return callback();
+		},
+		/**
+		 * [eventValidateField description]
+		 * @param  {[type]}   sublistId   [description]
+		 * @param  {[type]}   fieldId     [description]
+		 * @param  {[type]}   sublistLine [starts at 1]
+		 * @param  {Function} callback    [description]
+		 * @return {[type]}               [description]
+		 */
+		eventValidateField: function(sublistId, fieldId, sublistLine, callback) {
+			return callback(sublistId, fieldId, sublistLine);
+		},
+		/**
+		 * [eventFieldChanged description]
+		 * @param  {[type]}   sublistId   [description]
+		 * @param  {[type]}   fieldId     [description]
+		 * @param  {[type]}   sublistLine [starts at 1]
+		 * @param  {Function} callback    [description]
+		 * @return {[type]}               [description]
+		 */
+		eventFieldChanged: function(sublistId, fieldId, sublistLine, callback) {
+			return callback(sublistId, fieldId, sublistLine);
+		},
+		/**
+		 * [eventPostSourcing description]
+		 * @param  {[type]}   sublistId [description]
+		 * @param  {[type]}   fieldId   [description]
+		 * @param  {Function} callback  [description]
+		 * @return {[type]}             [description]
+		 */
+		eventPostSourcing: function(sublistId, fieldId, callback) {
+			return callback(sublistId, fieldId);
+		},
+		/**
+		 * [eventLineInit description]
+		 * @param  {[type]}   sublistId [description]
+		 * @param  {Function} callback  [description]
+		 * @return {[type]}             [description]
+		 */
+		eventLineInit: function(sublistId, callback) {
+			return callback(sublistId);
+		},
+		/**
+		 * [eventValidateLine description]
+		 * @param  {[type]}   sublistId [description]
+		 * @param  {Function} callback  [description]
+		 * @return {[type]}             [description]
+		 */
+		eventValidateLine: function(sublistId, callback) {
+			return callback(sublistId);
+		},
+		/**
+		 * [eventRecalc description]
+		 * @param  {[type]}   sublistId [description]
+		 * @param  {Function} callback  [description]
+		 * @return {[type]}             [description]
+		 */
+		eventRecalc: function(sublistId, callback) {
+			return callback(sublistId);
+		},
+		/**
+		 * [eventValidateInsert description]
+		 * @param  {[type]}   sublistId [description]
+		 * @param  {Function} callback  [description]
+		 * @return {[type]}             [description]
+		 */
+		eventValidateInsert: function(sublistId, callback) {
+			return callback(sublistId);
+		},
+		/**
+		 * [eventValidateDelete description]
+		 * @param  {[type]}   sublistId [description]
+		 * @param  {Function} callback  [description]
+		 * @return {[type]}             [description]
+		 */
+		eventValidateDelete: function(sublistId, callback) {
+			return callback(sublistId);
+		}
+	}
+};
